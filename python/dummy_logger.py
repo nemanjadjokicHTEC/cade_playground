147e2c8b36f1feb95b568c7564f77d73b09827f1
Tanja Djuric <tanja.djuric@htecgroup.com>
2025-12-04 19:19:23 +0100

    Changed Dockerfile to use dummy_logger script

--- python/dummy_logger.py
+++ python/dummy_logger.py
@@ -2,5 +2,5 @@ import subprocess
 from python.random_print_library.random_print_library import get_random_word
 
 
-output = subprocess.check_output(["/Working_Directory/timestamp_provider"]).decode('ascii').rstrip()
+output = subprocess.check_output(["/app/timestamp_provider"]).decode('ascii').rstrip()
 print(f"{output}: {get_random_word()}")
